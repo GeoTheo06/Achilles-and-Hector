bcf667f40b8ed4612a49e352bd446d42d23d629d
GeoTheo06 <theodorgeor@gmail.com>
2023-05-01 12:04:52 +0300

    0.3 seconds for nonDefence was not stable enough

--- Assets/Scripts/achileas.cs
+++ Assets/Scripts/achileas.cs
@@ -41,7 +41,7 @@ public class achileas : MonoBehaviour
 
 	IEnumerator attackTimer()
 	{
-		yield return new WaitForSeconds(0.3f);
+		yield return new WaitForSeconds(1);
 
 		achilAnimator.SetBool("attack", true);
 		gameScript.makeNonInteractable();
--- Assets/Scripts/ektoras.cs
+++ Assets/Scripts/ektoras.cs
@@ -43,7 +43,7 @@ public class ektoras : MonoBehaviour
 
 	IEnumerator attackTimer()
 	{
-		yield return new WaitForSeconds(0.3f);
+		yield return new WaitForSeconds(1);
 
 		ektorAnimator.SetBool("attack", true);
 		gameScript.makeNonInteractable();
